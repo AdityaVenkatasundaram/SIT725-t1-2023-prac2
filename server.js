dc9d296c719910c5c2914fc384851d1bbb910659
AdityaVenkatasundaram <85490617+AdityaVenkatasundaram@users.noreply.github.com>
2023-03-17 16:20:23 +1100

    Bootstrapping Express App​
    Creating private server and performing basic math operations

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get('/', function(req,res) {
+    res.render('index.html');
+});
+
+function Addition(num1, num2) {
+    return parseInt(num1) + parseInt(num2);
+}
+function Subtraction(num1, num2) {
+    return parseInt(num1) - parseInt(num2);
+}
+
+function Multiplication(num1, num2) {
+    return parseInt(num1) * parseInt(num2);
+}
+function Division(num1, num2) {
+    return parseInt(num1) / parseInt(num2);
+}
+
+function Modulus(num1, num2) {
+    return parseInt(num1) % parseInt(num2);
+}
+
+app.get('/addTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = Addition(number1, number2);
+    res.json({statusCode:200, data:result, message: 'Success'});
+});
+
+app.listen(port, () => {
+    console.log('App listening to: '+ port);
+});
