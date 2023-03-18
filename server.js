1eaeeb60c5cd3e80b9663540aa6ca14d42b6cf9f
AdityaVenkatasundaram <85490617+AdityaVenkatasundaram@users.noreply.github.com>
2023-03-18 13:01:11 +1100

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,35 @@ app.get('/addTwoNumbers', (req, res) => {
     res.json({statusCode:200, data:result, message: 'Success'});
 });
 
+app.get('/subractTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = Subraction(number1, number2);
+    res.json({statusCode:200, data:result, message: 'Success'});
+});
+
+app.get('/multiplyTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = Multiplication(number1, number2);
+    res.json({statusCode:200, data:result, message: 'Success'});
+});
+
+app.get('/divideTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = Division(number1, number2);
+    res.json({statusCode:200, data:result, message: 'Success'});
+});
+
+app.get('/moduloTwoNumbers', (req, res) => {
+    var number1 = req.query.number1;
+    var number2 = req.query.number2;
+    var result = Modulus(number1, number2);
+    res.json({statusCode:200, data:result, message: 'Success'});
+});
+
+
 app.listen(port, () => {
     console.log('App listening to: '+ port);
 });
